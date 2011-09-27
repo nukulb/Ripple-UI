89c6d02dbdb6f879da590b4d8c2298c6b183b952
Gord Tanner (macbook) <gord@tinyhippos.com>
2011-09-27 11:18:32 -0400

    refactoring / testing of honeypot and added contacts.save bridge call

--- test/unit/honeypot.js
+++ test/unit/honeypot.js
@@ -13,20 +13,55 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-xdescribe("honeypot", function () {
+describe("honeypot", function () {
     var honeypot = require('ripple/honeypot');
 
-    it("doesn't assign anything to the set value", function () {
-        honeypot.set(window, "bar", jasmine.createSpy());
-        expect(window.bar).not.toBeDefined();
-    });
+    describe("when monitoring", function () {
+        describe("with andReturn", function () {
+            it("doesn't create the property before accessed", function () {
+                var foo = {};
+                honeypot.monitor(foo, "bar").andReturn("mine");
+                expect(foo.bar).not.toBeDefined();
+            });
 
-    it("sets off the trap", function () {
-        var pooh = { oh: "bother" },
-            trap = jasmine.createSpy("trap");
+            it("returns the value rather than what was set", function () {
+                var orange = {};
+                honeypot.monitor(orange, "lantern").andReturn("mine");
+                orange.lantern = "oath";
+                expect(orange.lantern).toBe("mine");
+            });
+        });
 
-        honeypot.set(window, "foo", function () {}, trap);
-        window.foo = pooh;
-        expect(trap).toHaveBeenCalledWith(pooh);
+        describe("with andRun", function () {
+            it("doesn't create the property before accessed", function () {
+                var foo = {},
+                    get = jasmine.createSpy().andReturn("asdf");
+                honeypot.monitor(foo, "bar").andRun(get);
+
+                expect(get).not.toHaveBeenCalled();
+                expect(foo.bar).not.toBeDefined();
+            });
+
+            it("runs the get function when accessing", function () {
+                var eat = {},
+                    love = jasmine.createSpy("get func").andReturn("love");
+
+                honeypot.monitor(eat, "pray").andRun(love);
+                eat.pray = "and write a book";
+                expect(eat.pray).toBe("love");
+                expect(love).toHaveBeenCalled();
+            });
+
+            it("runs the set function when setting", function () {
+                var dude = {},
+                    sweet = jasmine.createSpy("set func");
+
+                honeypot.monitor(dude, "sweet").andRun(jasmine.createSpy(), sweet);
+                expect(sweet).not.toHaveBeenCalled();
+                dude.sweet = "asdf";
+
+                expect(sweet).toHaveBeenCalledWith("asdf");
+            });
+        });
     });
 });
